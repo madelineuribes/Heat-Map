3b5b63b93f71b8d8eedd486077322a1fd8c22a74
Madeline Uribes <madeline.uribes@gmail.com>
2020-09-06 17:03:07 -0400

    Adding color scale

--- heatmap.js
+++ heatmap.js
@@ -4,6 +4,12 @@ svg
   .attr("width", 800)
   .attr("height", data.length * 150)
 
+// -10C is 14F, 0 is 32, 7 is 45
+const colorScale = d3.scaleLinear()
+  .domain([-10, 0, 7, 14, 21, 24])
+  .range(["#814ee7", "#3f24ec", "#79e87C", "#fbe157", "#ff9737", "#fe3b3b"])
+
+
 const dataPoints = svg
   .selectAll("g.data-point")
   .data(data)
@@ -24,4 +30,25 @@ dataPoints
   .attr("x", 175)
   .attr("y", 100)
   .attr("class", "country")
-  .text((d, i) => { return d.country })    
+  .text((d, i) => { return d.country })
+
+const months = dataPoints
+  .append("g")
+  .attr("class", "months")
+  .attr("transform", "translate(200, 0)")
+
+const monthGroups = months
+  .selectAll("g.month")
+  .data((d, i) => { return d.months })
+  .enter()
+  .append("g")
+  .attr("class", "month")
+  .attr("transform", (d, i) => { return `translate(${i * 50}, 0)` })
+
+monthGroups
+  .append("rect")
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("width", 50)
+  .attr("height", 150)
+  .style("fill", (d, i) => { return colorScale(d) })  
